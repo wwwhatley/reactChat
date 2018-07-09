6f73e3b1ebd8051d21753f9c52f0028f8d076b86
William Whatley <william@catatek.com>
2018-07-09 18:38:52 -0400

    Configured webpack from ground up.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
-  "output": {
-    "filename": "[name].pack.js"
-  },
-  "module": {
-    "rules": [
+  module: {
+    rules: [
       {
-        "use": {
-          "loader": "babel-loader",
-          "options": {
-            "presets": [
-              "babel-preset-env",
-              "babel-preset-react"
-            ]
-          }
-        },
-        "exclude": /node_modules/,
-        "test": /\.js$/
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
-  "entry": {
-    "index": "./index"
-  }
+  plugins: [htmlWebpackPlugin]
 };
