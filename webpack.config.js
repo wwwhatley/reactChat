cce75286712f767f1289b1c0c8a622985062634a
William Whatley <william@catatek.com>
2018-07-10 14:30:09 -0400

    yarn config.

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
       }
     ]
   },
